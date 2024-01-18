d0d0d26536f6398068511d59ebb65fec9e2e1c03
Isto Nikula <isto.nikula@iki.fi>
2024-01-18 11:04:42 +0200

    runner: proto cmd

--- rw.sh
+++ rw.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 usage() {
   echo "Usage:"
   echo "  rw.sh -h       Display this help message"
+  echo "  rw.sh proto    Parse proto files and generate go files"
   echo "  rw.sh lint     Lint modules"
   echo "  rw.sh test     Test modules"
   echo "  rw.sh test -v  Test modules, verbose mode"
@@ -30,6 +31,20 @@ popd() {
 
 dir() {
   echo "$(cd "$(dirname "$1")" ; pwd -P)"
+}
+
+proto_cmd() {
+  local protos
+  readarray -d '' protos < <(find . -name *.proto -print0)
+  for p in "${protos[@]}"; do
+    pushd "$(dir "$p")"
+    local f="$(basename "$p")"
+    protoc \
+      --go_out=. --go_opt=paths=source_relative \
+      --go_opt=paths=source_relative --go_opt=paths=source_relative \
+      "$f"
+    popd
+  done
 }
 
 lint_cmd() {
@@ -99,6 +114,9 @@ shift $((OPTIND -1))
 
 CMD="$1"; shift
 case "$CMD" in
+  proto)
+    proto_cmd
+    ;;
   lint)
     run_in_module_dir lint_cmd
     ;;
